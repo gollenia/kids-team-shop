b625fc4a4590901b22970ef7b62a1a55396584a9
Thomas Gollenia <thomas@gollenia.at>
2020-12-11 09:58:54 +0100

    :bug: Twig data was object, not array

--- lib/Controllers/Edit.php
+++ lib/Controllers/Edit.php
@@ -7,11 +7,12 @@ use Contexis\Twig\Renderer;
 
 
 class Edit extends Controller {
+
     public function __construct($site) {
         parent::__construct($site);
     }
 
     public function render() {
-        echo Renderer::compile("pages/edit.twig", $this->site);
+        echo Renderer::compile("pages/edit.twig", $this->site->get());
     }
 }
