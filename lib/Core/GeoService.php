f2a4ae8cde11318d77df98d80945d1ffd6286eeb
Thomas Gollenia <thomas@gollenia.at>
2020-12-14 15:20:44 +0100

    Not needed

--- lib/Core/GeoService.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Contexis\Core;
-
-class GeoService {
-    static function get_location() {
-        return var_export(unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$_SERVER['REMOTE_ADDR'])));
-    }
-}
