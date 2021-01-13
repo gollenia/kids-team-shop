8f561d0705efacde86ef3a8624575afa971d34a6
Thomas Gollenia <thomas@gollenia.at>
2021-01-13 14:09:11 +0100

    Added some globals

--- config/routes.php
+++ config/routes.php
@@ -1,7 +1,10 @@
 <?php
 global $ID;
+global $INPUT;
+global $ACT;
 
 return [
+    "\\Contexis\\Controllers\\Search" => $ACT == "search",
     "\\Contexis\\Controllers\\MenuEdit" => $ID == "system:menu",
-    "\\Contexis\\Controllers\\BibleView" => explode(":", $ID)[0] == "bibel"
+    "\\Contexis\\Controllers\\BibleView" => explode(":", $ID)[0] == "bibel",    
 ];
