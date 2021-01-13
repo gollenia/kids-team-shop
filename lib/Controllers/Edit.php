f662a40075b54707fa0c2ce853a2816bb6c48bc2
Thomas Gollenia <thomas@gollenia.at>
2021-01-13 14:14:17 +0100

    Ajax-Catch

--- lib/Controllers/Edit.php
+++ lib/Controllers/Edit.php
@@ -8,7 +8,11 @@ use Contexis\Twig\Renderer;
 
 class Edit extends Controller {
 
-    public function __construct($site) {
+    public function __construct($site, $is_ajax) {
+        if($is_ajax) {
+            header('HTTP/1.0 403 Forbidden'); 
+            die();
+        }
         parent::__construct($site);
     }
 
