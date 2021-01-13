fd5494e868e181f055cd5f490a90a566eba85ee7
Thomas Gollenia <thomas@gollenia.at>
2021-01-13 14:15:56 +0100

    Ajax Catch

--- lib/Controllers/Show.php
+++ lib/Controllers/Show.php
@@ -9,8 +9,12 @@ use Contexis\Core\Site;
 
 class Show extends Controller {
 
-    public function __construct($site) {
-        parent::__construct($site);
+    public function __construct($site, $is_ajax) {
+        if($is_ajax) {
+            header('HTTP/1.0 403 Forbidden'); 
+            die();
+        }
+        parent::__construct($site, $is_ajax);
     }
 
     public function render() {
