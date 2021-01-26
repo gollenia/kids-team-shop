cf275f20905c0b7fae2bbd8ac38e56bdaddc7f40
Thomas Gollenia <thomas@gollenia.at>
2021-01-26 17:02:11 +0100

    MenuEdit didn't work

--- lib/Controllers/MenuEdit.php
+++ lib/Controllers/MenuEdit.php
@@ -3,14 +3,20 @@
 namespace Contexis\Controllers;
 
 use Contexis\Core\Controller;
+use Contexis\Twig\Renderer;
 
 
-class MenuEdit extends Controller {
-    public function __construct($page) {
-        parent::__construct($page);
+class Edit extends Controller {
+
+    public function __construct($site, $is_ajax) {
+        if($is_ajax) {
+            header('HTTP/1.0 403 Forbidden'); 
+            die();
+        }
+        parent::__construct($site, $is_ajax);
     }
 
     public function render() {
-        echo "editing";
+        echo Renderer::compile("pages/edit.twig", $this->site->get());
     }
 }
