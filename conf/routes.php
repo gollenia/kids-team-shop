4ecbb4f68cb4778655dee5eff5f1c776e820cfc8
Thomas Gollenia <thomas@gollenia.at>
2021-02-05 18:26:42 +0100

    config files moved to /conf for Dokuwiki compatibility

---
+++ conf/routes.php
@@ -0,0 +1,11 @@
+<?php
+global $ID;
+global $INPUT;
+global $ACT;
+
+return [
+    "\\Contexis\\Controllers\\Search" => $ACT == "search",
+    "\\Contexis\\Controllers\\MenuEdit" => $ID == "system:menu",
+    "\\Contexis\\Controllers\\BibleView" => explode(":", $ID)[0] == "bibel" && $ACT == "show",    
+    "\\Contexis\\Controllers\\Tag" => explode(":", $ID)[0] == "tag"    
+];
