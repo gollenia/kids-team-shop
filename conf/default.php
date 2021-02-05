b8f5964a055eb05d56d050327a7121489416e10a
Thomas Gollenia <thomas@gollenia.at>
2021-02-05 18:27:05 +0100

    Dokuwiki Theme settings

---
+++ conf/default.php
@@ -0,0 +1,4 @@
+<?php
+$conf['excludepages'] = "bibel,system";
+$conf['menu'] = "system:menu";
+$conf['templates'] = "tystem:templates";
---
+++ conf/metadata.php
@@ -0,0 +1,4 @@
+<?php
+$meta['excludepages'] = array("string", '_pattern' => '#^[a-z:]*#');
+$meta['menu'] = array("string", '_pattern' => '#^[a-z:]*#');
+$meta['templates'] = array("string", '_pattern' => '#^[a-z:]*#');
