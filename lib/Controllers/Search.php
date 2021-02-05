bfe6ec6bb56b00160ecebabd2585a58f789231d3
Thomas Gollenia <thomas@gollenia.at>
2021-02-05 18:30:35 +0100

    Old AJAX lock removed

--- lib/Controllers/Search.php
+++ lib/Controllers/Search.php
@@ -3,24 +3,14 @@
 namespace Contexis\Controllers;
 
 use Contexis\Core\Controller;
-use Contexis\Twig\Renderer;
-use Contexis\Core\Site;
 
 class Search extends Controller {
 
-    public function __construct($site, $is_ajax) {
-        parent::__construct($site, $is_ajax);
-    }
+    public $template = "search";
 
-    public function render() {
-        
-        global $ACT;
-        
-        global $INPUT;
-
-        return Renderer::compile("pages/search.twig", $this->site->get());
+    public function __construct($site) {
+        parent::__construct($site);
     }
-	
 }
 
 
--- lib/Controllers/Show.php
+++ lib/Controllers/Show.php
@@ -3,21 +3,8 @@
 namespace Contexis\Controllers;
 
 use Contexis\Core\Controller;
-use Contexis\Twig\Renderer;
-use Contexis\Core\Site;
-
 
 class Show extends Controller {
 
-    public function __construct($site, $is_ajax) {
-        if($is_ajax) {
-            header('HTTP/1.0 403 Forbidden'); 
-            die();
-        }
-        parent::__construct($site, $is_ajax);
-    }
-
-    public function render() {
-        echo Renderer::compile("pages/show.twig", $this->site->get());
-    }
+    
 }
