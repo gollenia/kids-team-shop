035287bed03413959daa90ebd6935e241a1c8dbc
Thomas Gollenia <thomas@gollenia.at>
2021-02-05 18:32:58 +0100

    code added from main.php

--- lib/Core/Controller.php
+++ lib/Core/Controller.php
@@ -3,12 +3,12 @@
 namespace Contexis\Core;
 
 use Contexis\Core\Site;
-
+use Contexis\Twig\Renderer;
 
 
 interface ControllerInterface {
     
-    public function __construct(Site $site, $is_ajax);
+    public function __construct(Site $site);
     public function render();
 }
 
@@ -16,17 +16,59 @@ interface ControllerInterface {
 class Controller implements ControllerInterface {
 
     public Site $site;
+    public $template = "show";
 
-    public function __construct(Site $site, $is_ajax)
+    public function __construct(Site $site)
     {
         $this->site = $site;
     }
 
+    /**
+     * Default renderer function
+     *
+     * @return string renderer result
+     */
     public function render() {
-        echo "No rendering method implemented";
+        $template = $this->template;
+
+        if (empty($template)) {
+            $template = strtolower(end(explode("\\", get_class($this))));
+        }
+        
+        return Renderer::compile("pages/{$template}.twig", $this->site->get());
     }
 
-    private function get_act_controller() {
+    public static function get($route, $site) {
 
+        global $INPUT;
+        
+
+        if($INPUT->str('controller',false)) {
+        
+            $controllerName = ucfirst ( $INPUT->str('controller'));
+            $controllerName = "Contexis\\Controllers\\" . $controllerName;
+            $controller = new $controllerName($site, true);
+            $method = $INPUT->str('method', 'get');
+            
+            if (method_exists( $controller, "ajax_" . $method)) {
+                    header('Content-Type: application/json');
+                    echo call_user_func_array ( array($controller, "ajax_" . $method), [$INPUT]); // , [$INPUT]
+                    
+            } else {
+                    header('Content-Type: application/json');
+                    echo "{error: 'method " . $method . " in controller " . $controllerName . " not found'}";
+                    
+            }
+    } else {
+    
+            if (!class_exists($route)) {
+                    //$route = "\\Contexis\\Core\\Controller";
+            }
+            $controller = new $route($site);
+            echo $controller->render($twig);
+            tpl_indexerWebBug();
+    }
+    
+    
     }
 }
