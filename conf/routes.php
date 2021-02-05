f40342cb898734a65be570ac25566d868e70835b
Thomas Gollenia <thomas@gollenia.at>
2021-02-05 19:08:36 +0100

    Tag controller only on ACT == show

--- conf/routes.php
+++ conf/routes.php
@@ -7,5 +7,5 @@ return [
     "\\Contexis\\Controllers\\Search" => $ACT == "search",
     "\\Contexis\\Controllers\\MenuEdit" => $ID == "system:menu",
     "\\Contexis\\Controllers\\BibleView" => explode(":", $ID)[0] == "bibel" && $ACT == "show",    
-    "\\Contexis\\Controllers\\Tag" => explode(":", $ID)[0] == "tag"    
+    "\\Contexis\\Controllers\\Tag" => explode(":", $ID)[0] == "tag" && $ACT == "show"        
 ];
