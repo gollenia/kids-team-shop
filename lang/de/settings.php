651a7ac80590cbf1d7ac95fff4e8e8fe6fed097f
Thomas Gollenia <thomas@gollenia.at>
2021-02-05 18:27:17 +0100

    Translation file for Theme settings

---
+++ lang/de/settings.php
@@ -0,0 +1,4 @@
+<?php
+$lang['excludepages']               = 'Seiten, die im Seitenbaum ausgeblendet werden';
+$lang['menu']                       = 'Seite, auf der dasd Menü gespeichert ist';
+$lang['templates']                  = 'Namespace, in dem die Templates gespeichert werden';
