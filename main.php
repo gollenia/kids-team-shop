f9ca2dda7a379818d44ae22d130a7603228ebd73
Thomas Gollenia <thomas@gollenia.at>
2021-02-05 18:25:02 +0100

    Controller Part moved to Controller Class

--- main.php
+++ main.php
@@ -5,7 +5,8 @@ require __DIR__ . '/vendor/autoload.php';
 use Contexis\Core\{
         Site,
         Router,
-        Config
+        Config,
+        Controller
 };
 
 
@@ -15,29 +16,7 @@ global $INPUT;
 $site = new Site();
 $route = new Router(Config::load("routes"));
 
-if($INPUT->str('controller',false)) {
-        
-        $controllerName = ucfirst ( $INPUT->str('controller'));
-	$controllerName = "Contexis\\Controllers\\" . $controllerName;
-	$controller = new $controllerName($site, true);
-	$method = $INPUT->str('method', 'get');
-        
-        if (method_exists( $controller, $method)) {
-                header('Content-Type: application/json');
-                echo call_user_func_array ( array($controller, $method), []); // , [$INPUT]
-                
-        } else {
-                header('Content-Type: application/json');
-                echo "{error: 'method " . $method . " in controller " . $controllerName . " not found'}";
-                
-        }
-} else {
-
-        $controllerClass = $route->get();
-        $controller = new $controllerClass($site, false);
-        echo $controller->render($twig);
-        tpl_indexerWebBug();
-}
+$controller = Controller::get($route->get(), $site);
 
 
 
