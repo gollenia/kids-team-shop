af332ea4e23c6c0e1913fb9d54ea9efe02457d86
Thomas Gollenia <thomas@gollenia.at>
2021-02-17 12:00:51 +0100

    folder icons

--- resources/vue/LinkPickerItem.vue
+++ resources/vue/LinkPickerItem.vue
@@ -1,8 +1,9 @@
 <template>
     <div v-if="item.folder" class="link-folder">
-        <div class=" flex items-center" @click="expanded = !expanded">
-            <span class="link-folder-icon"><i class="material-icons">{{ expanded ? 'arrow_drop_down' : 'arrow_right' }}</i></span>
-            <span>{{ item.title || item.name }}</span>
+        <div class="text-gray flex items-center relative" @click="expanded = !expanded">
+            <span class="link-folder-icon cursor-pointer"><i class="material-icons">{{ expanded ? 'arrow_drop_down' : 'arrow_right' }}</i></span>
+            <span class="link-folder-icon cursor-pointer absolute ml-6"><i class="material-icons">folder</i></span>
+            <span class="pl-8 text-sm">{{ item.title || item.name }}</span>
         </div>
         <ul v-if="expanded">
             <li v-for="(child, c) in item.children" :key="c">
@@ -10,8 +11,9 @@
             </li>
         </ul>
     </div>
-    <div v-else class="link-item" @click="$emit('select', item)">
-        {{ item.title || item.name }}
+    <div v-else class="text-darkgray link-item flex items-center relative" @click="$emit('select', item)">
+        <span class="link-folder-icon cursor-pointer absolute"><i class="material-icons">text_snippet</i></span>
+        <span class="pl-8 text-sm">{{ item.title || item.name }}</span>
     </div>
 </template>
 
@@ -39,6 +41,5 @@ export default {
 }
 .link-item {
     cursor: pointer;
-    text-decoration: underline;
 }
 </style>
