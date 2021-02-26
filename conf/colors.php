9588fe16744470dda1052f05e4023e040698e9a8
Thomas Gollenia <thomas@gollenia.at>
2021-02-26 13:42:26 +0100

    removed old colors

--- conf/colors.php
+++ conf/colors.php
@@ -1,24 +1,7 @@
 <?php
 
 return [
-    [
-        "color" => "#dddddd",
-        "name" => "Hellgrau",
-        "slug" => "lightgray",
-        "dark" => false
-    ],
-    [
-        "color" => "#999999",
-        "name" => "Mittelgrau",
-        "slug" => "gray",
-        "dark" => false
-    ],
-    [
-        "color" => "#333333",
-        "name" => "Dunkelgrau",
-        "slug" => "darkgray",
-        "dark" => true
-    ],
+ 
     [
         "color" => "#009cd9",
         "name" => "Blau",
@@ -30,12 +13,6 @@ return [
         "name" => "Hellblau",
         "slug" => "lightblue",
         "dark" => false
-    ],
-    [
-        "color" => "#153f84",
-        "name" => "Dunkelblau",
-        "slug" => "darkblue",
-        "dark" => true
     ],
     [
         "color" => "#F17300",
@@ -50,12 +27,6 @@ return [
         "dark" => false
     ],
     [
-        "color" => "#a02200",
-        "name" => "Dunkel Orange",
-        "slug" => "darkorange",
-        "dark" => true
-    ],
-    [
         "color" => "#67B82A", //"#83BD4C",
         "name" => "Grün",
         "slug" => "green",
@@ -66,41 +37,5 @@ return [
         "name" => "Hellgrün",
         "slug" => "lightgreen",
         "dark" => false
-    ],
-    [
-        "color" => "#0b6127",
-        "name" => "Dunkelgrün",
-        "slug" => "darkgreen",
-        "dark" => true
-    ],
-    [
-        "color"=> "#8E53C0",
-        "name"=> "Lila",
-        "slug"=> "purple",
-        "dark"=> true
-    ],
-    [
-        "color"=> "#D3BCE6",   
-        "name"=> "Hell-Lila",
-        "slug"=> "lightpurple",
-        "dark"=> false
-    ],
-    [
-        "color"=> "#491675",   
-        "name"=> "Dunkel-Lila",
-        "slug"=> "darkpurple",
-        "dark"=> true
-    ],
-    [
-        "color"=> "#e63749",
-        "name"=> "Rot",
-        "slug"=> "red",
-        "dark"=> true
-    ],
-    [
-        "color"=> "#ffb4bb",
-        "name"=> "Hell-Rot",
-        "slug"=> "lightred",
-        "dark"=> false
-    ],
+    ]
 ];
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,7 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      colors:{
-
+      colors: {
         "primary": {
           contrast: "var(--primary-contrast)",
           transparent: "var(--primary-transparent)",
@@ -18,85 +17,13 @@ module.exports = {
           DEFAULT: "var(--primary)",
           darker: "var(--primary-darker)",
         },
-        "red": {
-          contrast: "var(--red-contrast)",
-          transparent: "var(--red-transparent)",
-          contrastsoft: "var(--red-soft-contrast)",
-          lighter: "var(--red-lighter)",
-          DEFAULT: "var(--red)",
-          darker: "var(--red-darker)",
-        },
-        "green": {
-          contrast: "var(--green-contrast)",
-          transparent: "var(--green-transparent)",
-          contrastsoft: "var(--green-soft-contrast)",
-          lighter: "var(--green-lighter)",
-          DEFAULT: "var(--green)",
-          darker: "var(--green-darker)",
-        },
-        "blue": {
-          contrast: "var(--blue-contrast)",
-          transparent: "var(--blue-transparent)",
-          contrastsoft: "var(--blue-soft-contrast)",
-          lighter: "var(--blue-lighter)",
-          DEFAULT: "var(--blue)",
-          darker: "var(--blue-darker)",
-        },
-        "orange": {
-          contrast: "var(--orange-contrast)",
-          transparent: "var(--orange-transparent)",
-          contrastsoft: "var(--orange-soft-contrast)",
-          lighter: "var(--orange-lighter)",
-          DEFAULT: "var(--orange)",
-          darker: "var(--orange-darker)",
-        },
-        "purple": {
-          contrast: "var(--purple-contrast)",
-          transparent: "var(--purple-transparent)",
-          contrastsoft: "var(--purple-soft-contrast)",
-          lighter: "var(--purple-lighter)",
-          DEFAULT: "var(--purple)",
-          darker: "var(--purple-darker)",
-        },
-        "primary-light": {
-          contrast: "var(--primary-light-contrast)",
-          transparent: "var(--primary-light-transparent)",
-          contrastsoft: "var(--primary-light-soft-contrast)",
-          lighter: "var(--primary-light-lighter)",
-          DEFAULT: "var(--primary-light)",
-          darker: "var(--primary-light-darker)",
-        },
-        "primary-dark": {
-          contrast: "var(--primary-dark-contrast)",
-          transparent: "var(--primary-dark-transparent)",
-          contrastsoft: "var(--primary-dark-soft-contrast)",
-          lighter: "var(--primary-dark-lighter)",
-          DEFAULT: "var(--primary-dark)",
-          darker: "var(--primary-dark-darker)",
-        },
-        "darkgray": {
-          contrast: "var(--darkgray-contrast)",
-          transparent: "var(--darkgray-transparent)",
-          contrastsoft: "var(--darkgray-soft-contrast)",
-          lighter: "var(--darkgray-lighter)",
-          DEFAULT: "var(--darkgray)",
-          darker: "var(--darkgray-darker)",
-        },
-        "lightgray": {
-          contrast: "var(--lightgray-contrast)",
-          transparent: "var(--lightgray-transparent)",
-          contrastsoft: "var(--lightgray-soft-contrast)",
-          lighter: "var(--lightgray-lighter)",
-          DEFAULT: "var(--lightgray)",
-          darker: "var(--lightgray-darker)",
-        },
-        "gray": {
-          contrast: "var(--gray-contrast)",
-          transparent: "var(--gray-transparent)",
-          contrastsoft: "var(--gray-soft-contrast)",
-          lighter: "var(--gray-lighter)",
-          DEFAULT: "var(--gray)",
-          darker: "var(--gray-darker)",
+        "secondary": {
+          contrast: "var(--secondary-contrast)",
+          transparent: "var(--secondary-transparent)",
+          contrastsoft: "var(--secondary-soft-contrast)",
+          lighter: "var(--secondary-lighter)",
+          DEFAULT: "var(--secondary)",
+          darker: "var(--secondary-darker)",
         }
       },
       fontFamily: {
@@ -104,16 +31,7 @@ module.exports = {
         body: ['"Exo 2"', 'sans-serif'],
         script: ['"Gochi Hand"', 'sans-serif'],
       },
-      borderRadius: {
-        'none': '0',
-         'sm': '5px',
-         DEFAULT: '0.25rem',
-         DEFAULT: '10px',
-         'md': '20px',
-         'lg': '50px',
-         'full': '9999px',
-         'large': '12px',
-      }
+      
     }
   },
   
