5e76a1fde0168e4d9dd9bdb7a6b04ac48ddc9c96
Thomas Gollenia <thomas@gollenia.at>
2021-02-26 09:58:33 +0100

    no real changes

--- lib/Controllers/Show.php
+++ lib/Controllers/Show.php
@@ -3,8 +3,18 @@
 namespace Contexis\Controllers;
 
 use Contexis\Core\Controller;
+use \Contexis\Models\Page;
+use \Contexis\Twig\Renderer;
 
 class Show extends Controller {
 
+    public function __construct($site) {
+        parent::__construct($site);
+        
+    }
+
+    
+
+    
     
 }
