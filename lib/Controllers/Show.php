b726415399c1d2e2727116c9bb3b99f779fc1a4f
Thomas Gollenia <thomas@gollenia.at>
2021-03-02 12:48:52 +0100

    Template insertion

--- lib/Controllers/Show.php
+++ lib/Controllers/Show.php
@@ -4,17 +4,32 @@ namespace Contexis\Controllers;
 
 use Contexis\Core\Controller;
 use \Contexis\Models\Page;
+use Contexis\Models\Template;
 use \Contexis\Twig\Renderer;
+use dokuwiki\Extension\Event;
 
 class Show extends Controller {
 
     public function __construct($site) {
         parent::__construct($site);
-        
+        $content = $this->get_content();
+        if($this->site->get('metadata')['template']) {
+            $content = Template::apply($this->site->get('metadata')['template'], $content, $this->site->get("id"));
+            
+        }
+        $this->site->add_data("content", $content);     
+    }
+
+    private function get_content() {
+        global $ACT;
+        ob_start();
+        Event::createAndTrigger('TPL_ACT_RENDER', $ACT, 'tpl_content_core');
+        Event::createAndTrigger('TPL_CONTENT_DISPLAY', $html_output, 'ptln');
+        $html_output = ob_get_clean();
+        return $html_output;
     }
 
     
-
-    
+   
     
 }
