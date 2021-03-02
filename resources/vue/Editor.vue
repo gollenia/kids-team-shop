03d959b56a6ba295427d49fa7338c0ff57f10000
Thomas Gollenia <thomas@gollenia.at>
2021-03-02 12:55:24 +0100

    New Codemirror implemented

--- resources/vue/Editor.vue
+++ resources/vue/Editor.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="bg-lightgray-lighter" v-shortkey="['esc']" @shortkey="showLinkPicker = false; showMediaPicker = false; showPageImagePicker = false">
-         <header class="bg-primary-light relative" style="padding-top: 20vh;">
-            <img class="object-cover absolute inset-0 w-full h-full" :src="media.src">
+    <div class="bg-gray-100" v-shortkey="['esc']" @shortkey="showLinkPicker = false; showMediaPicker = false; showPageImagePicker = false">
+         <header class="bg-secondary relative" style="padding-top: 20vh;">
+            <img class="object-cover absolute inset-0 w-full h-full" v-if="media.src != '/_media/'" :src="media.src">
             <div class="absolute bottom-0 right-0 text-right p-4">
             <button class="bg-lightgray text-black mr-4" @click="showPageImagePicker = true"><i class="material-icons">image</i></button>
             <button class="bg-lightgray text-black" @click="pageimage = ''"><i class="material-icons">delete</i></button>
@@ -18,15 +18,17 @@
         <media-picker v-if="showPageImagePicker" @select="insertPageImage($event); showPageImagePicker = false" @close="showPageImagePicker = false"/>
 
         <div class="max-w-screen-xl mx-auto px-4 md:px-8 xl:px-0 py-8">
-            <h5>Titel</h5>
+            
             <div class="input-text">
+                <label>Titel</label>
                 <input type="text" class="w-full border-2 outline-none" v-model="page.title" required>
             </div>
         </div>
 
         <div class="max-w-screen-xl mx-auto px-4 md:px-8 xl:px-0 py-8">
-            <h5>Vorlage</h5>
+            
             <div class="input-select">
+                <label>Vorlage</label>
                 <select type="text" class="w-full border-2 outline-none" v-model="page.template" required>
                     <option v-for="(template, c) in templates" :value="template.id" v-text="template.title" :key="c"></option>
                 </select>
@@ -34,42 +36,42 @@
         </div>
 
         <div class="editor max-w-screen-xl mx-auto px-4 md:px-8 xl:px-0 py-8">
-            <div class="mt-4 px-4 bg-lightgray rounded-tl-md">
+            <div class="mt-4 px-4 bg-gray-200 rounded-tl-md">
                 <div class="tool__bar" role="toolbar">
-                    <button class="toolbutton" title="Fetter Text [B]" @click="textWrap('**', '**')" v-shortkey.once="['ctrl', 'b']" @shortkey="textWrap('**', '**')">
+                    <button class="toolbutton button" title="Fetter Text [B]" @click="textWrap('**', '**')" v-shortkey.once="['ctrl', 'b']" @shortkey="textWrap('**', '**')">
                         <i class="material-icons">format_bold</i>
                     </button>
-                    <button class="toolbutton" title="Kursiver Text [I]" @click="textWrap('//', '//')" v-shortkey.once="['ctrl', 'i']" @shortkey="textWrap('//', '//')">
+                    <button class="toolbutton button" title="Kursiver Text [I]" @click="textWrap('//', '//')" v-shortkey.once="['ctrl', 'i']" @shortkey="textWrap('//', '//')">
                         <i class="material-icons">format_italic</i>
                     </button>
-                    <button class="toolbutton" title="Unterstrichener Text [U]" @click="textWrap('__', '__')" v-shortkey.once="['ctrl', 'i']" @shortkey="textWrap('__', '__')">
+                    <button class="toolbutton button" title="Unterstrichener Text [U]" @click="textWrap('__', '__')" v-shortkey.once="['ctrl', 'i']" @shortkey="textWrap('__', '__')">
                         <i class="material-icons">format_underlined</i>
                     </button>
-                    <button class="toolbutton" title="Überschrift 1" @click="textWrap('======', '======')">
+                    <button class="toolbutton button" title="Überschrift 1" @click="textWrap('======', '======')">
                         <i class="material-icons">filter_1</i>
                     </button>
-                    <button class="toolbutton" title="Überschrift 2" @click="textWrap('=====', '=====')">
+                    <button class="toolbutton button" title="Überschrift 2" @click="textWrap('=====', '=====')">
                         <i class="material-icons">filter_2</i>
                     </button>
-                    <button class="toolbutton" title="Überschrift3" @click="textWrap('====', '====')">
+                    <button class="toolbutton button" title="Überschrift3" @click="textWrap('====', '====')">
                         <i class="material-icons">filter_3</i>
                     </button>
-                    <button class="toolbutton" title="Interner Link [L]" @click="showLinkPicker = true" v-shortkey.once="['ctrl', 'l']" @shortkey="showLinkPicker = true">
+                    <button class="toolbutton button" title="Interner Link [L]" @click="showLinkPicker = true" v-shortkey.once="['ctrl', 'l']" @shortkey="showLinkPicker = true">
                         <i class="material-icons">insert_link</i>
                     </button>
-                    <button class="toolbutton" title="Externer Link"  @click="textWrap('[[', ']]', '[[https://www.example.com|Externer Link]]')">
+                    <button class="toolbutton button" title="Externer Link"  @click="textWrap('[[', ']]', '[[https://www.example.com|Externer Link]]')">
                         <i class="material-icons">explore</i>
                     </button>
-                    <button class="toolbutton" title="Nummerierter Listenpunkt [-]" @click="textWrap(' - ', '', ' - Nummerierter Listenpunkt')" v-shortkey.once="['ctrl', '-']" @shortkey="textWrap(' - ', '', ' - Nummerierter Listenpunkt')">
+                    <button class="toolbutton button" title="Nummerierter Listenpunkt [-]" @click="textWrap(' - ', '', ' - Nummerierter Listenpunkt')" v-shortkey.once="['ctrl', '-']" @shortkey="textWrap(' - ', '', ' - Nummerierter Listenpunkt')">
                         <i class="material-icons">format_list_numbered</i>
                     </button>
-                    <button class="toolbutton" title="Listenpunkt [.]" @click="textWrap(' * ', '', ' * Listenpunkt')" v-shortkey.once="['ctrl', '.']" @shortkey="textWrap(' * ', '', ' - Listenpunkt')">
+                    <button class="toolbutton button" title="Listenpunkt [.]" @click="textWrap(' * ', '', ' * Listenpunkt')" v-shortkey.once="['ctrl', '.']" @shortkey="textWrap(' * ', '', ' - Listenpunkt')">
                         <i class="material-icons">format_list_bulleted</i>
                     </button>
-                    <button class="toolbutton" title="Horizontale Linie" aria-controls="wiki__text"@click="textWrap('\n----\n')">
+                    <button class="toolbutton button" title="Horizontale Linie" aria-controls="wiki__text"@click="textWrap('\n----\n')">
                         <i class="material-icons">horizontal_rule</i>
                     </button>
-                    <button class="toolbutton" title="Bilder und andere Dateien hinzufügen"  @click="showMediaPicker = true">
+                    <button class="toolbutton button" title="Bilder und andere Dateien hinzufügen"  @click="showMediaPicker = true">
                         <i class="material-icons">insert_photo</i>
                     </button>
                 </div>
@@ -78,18 +80,20 @@
             <codemirror class="border-r-2 border-l-2 border-b-2 p-4 rounded-br-md bg-white" ref="cm" v-model="page.content" :options="cmOptions" @keyHandled="cmOnKeyHandle($event)"></codemirror>
         </div>
 
-        <div class="max-w-screen-xl mx-auto px-4 md:px-8 xl:px-0 py-8">
-            <h5>Zusammenfassung</h5>
+        <div class="input-text max-w-screen-xl mx-auto px-4 md:px-8 xl:px-0 py-8">
+            
             <div class="editor-tags">
+                <label>Zusammenfassung</label>
                 <textarea class="w-full border-2 border-lightgray rounded-tl-md p-4 rounded-br-md outline-none" v-model="page.abstract"></textarea>
             </div>
         </div>
         
         
         
-        <div class="max-w-screen-xl mx-auto px-4 md:px-8 xl:px-0 py-8">
-            <h5>Schlagworte</h5>
-            <div class="editor-tags">
+        <div class="input-text max-w-screen-xl mx-auto px-4 md:px-8 xl:px-0 py-8">
+            
+            <div class="editor-tags input-text">
+                <label>Schlagworte</label>
                 <input-tag v-model="page.tags" :before-adding="tag => tag.toLowerCase().replace(' ', '_')" placeholder="Tag hinzufügen"></input-tag>
             </div>
         </div>
@@ -135,7 +139,7 @@ export default {
             mode: {
                 name: 'tiki'
             },
-            theme: 'base16-dark',
+            
             lineNumbers: false,
             line: false,
             lineWrapping: true
