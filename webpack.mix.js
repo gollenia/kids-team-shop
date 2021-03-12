fdef8a318a2fe1cc299e8d1173cba9c799bf64f5
Thomas Gollenia <thomas@gollenia.at>
2021-03-12 10:29:49 +0100

    autoprefixer added

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,7 @@ let mix = require('laravel-mix');
 mix.js('resources/js/main.js', 'public/js')
     .postCss('resources/css/main.css', 'public/css', [
         require('tailwindcss'),
+        require('autoprefixer')
     ]);
 
 mix.js('resources/js/editor.js', 'public/js').vue();
