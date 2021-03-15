5f2ade4705cec100a4f9f9a86d336c3f9875b946
Thomas Gollenia <thomas@gollenia.at>
2021-03-15 07:50:11 +0100

    added new template tags

--- lib/Controllers/Tag.php
+++ lib/Controllers/Tag.php
@@ -3,6 +3,7 @@
 namespace Contexis\Controllers;
 
 use Contexis\Core\Controller;
+use Contexis\Database\Index;
 use Contexis\Models\Page;
 use Contexis\Twig\Renderer;
 
@@ -12,7 +13,16 @@ class Tag extends Controller {
 
     public function __construct($site) {
         global $ID;
+
         parent::__construct($site);
+
+        $path = explode(":", $ID);
+        if(count($path) == 1) {
+            $this->template = "tags";
+            $this->site->add_data("tags", Index::getMetaValues("subject"));
+            return;
+        }
+        
         $tag = end(explode(":", $ID));
         $pages = Page::where("tag", $tag);
         $this->site->add_data("pages", $pages);
@@ -22,7 +32,7 @@ class Tag extends Controller {
     
 
     public function render() {
-        echo Renderer::compile("pages/tag.twig", $this->site->get());
+        echo Renderer::compile("pages/" . $this->template . ".twig", $this->site->get());
     }
 }
 
